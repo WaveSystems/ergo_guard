6941265335d1f21213d155be20a84719b42731ff
Antonio Chavez <cavjzz@gmail.com>
2012-05-06 23:05:02 -0500

    Added system tray class, only remains the icon for the system tray

---
+++ src/ergo_guard/system_tray.java
@@ -0,0 +1,35 @@
+package ergo_guard;
+
+import java.util.*;
+import java.awt.*; 
+import java.awt.event.*; 
+
+public class SystemTray {
+
+  private SystemTray tray;
+  private Image img;
+  private PopupMenu popup;
+  private MenuItem exit;
+
+  public SystemTray(){
+    tray = SystemTray.getSystemTray();
+    img = Toolkit.getDefaultToolkit().getImage("c:/Java/Apps/gui/tray.png");
+    popup = new PopupMenu();
+    exit = new MenuItem("Exit");
+
+    exit.addActionListener(new ActionListener() {
+      System.exit(0);
+    }
+    popup.add(exit);
+    TrayIcon trayIcon = new TrayIcon(img, "Tray Demo", popup);
+    try {
+      tray.add(trayIcon);
+    } catch (AWTException e) {
+      System.err.println("Problem loading Tray icon");
+    }
+  }
+
+  public void run() {
+    new SystemTray();
+  }
+}
