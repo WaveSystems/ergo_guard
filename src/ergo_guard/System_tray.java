2720999f5b5e36524c253492b32ea99f4079533f
Alberto Alcaraz <jaalcarazz@yahoo.com>
2012-05-09 20:09:34 -0500

    Pause element added

--- src/ergo_guard/System_tray.java
+++ src/ergo_guard/System_tray.java
@@ -10,12 +10,14 @@ public class System_tray{
   private Image img;
   private PopupMenu popup;
   private MenuItem ex;
+  private MenuItem pause;
 
   public System_tray(){
     tray = SystemTray.getSystemTray();
     img = Toolkit.getDefaultToolkit().getImage("imgs/system_tray/system_tray.png");
     popup = new PopupMenu();
     ex = new MenuItem("Exit");
+    pause = new MenuItem("Pause");
   }
 
   public void create_tray_icon() {
@@ -25,11 +27,24 @@ public class System_tray{
       public void actionPerformed(ActionEvent evt) {
         System.exit(0);
       }
+    });
+    
+    pause.addActionListener(new ActionListener() {
 
+      public void actionPerformed(ActionEvent evt) {
+          if(!Ergo_guard.active){
+              pause.setLabel("Continue");
+              Ergo_guard.active = true;
+          }else{
+              pause.setLabel("Pause");
+              Ergo_guard.active = false;
+          }
+      }
     });
 
+    popup.add(pause);
     popup.add(ex);
-    TrayIcon trayIcon = new TrayIcon(img, "Tray Demo", popup);
+    TrayIcon trayIcon = new TrayIcon(img, "Ergo-guard", popup);
     try {
       tray.add(trayIcon);
     } catch (AWTException e) {
